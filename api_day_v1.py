72bf2466786c3a7aabe44767a8f8a4b0f5a19c83
brushnit <144825928+brushnit@users.noreply.github.com>
2025-09-02 18:25:50 -0500

    Added search options (boundary, streets, buildings)

--- api_day_v1.py
+++ api_day_v1.py
@@ -1,12 +1,28 @@
 ### SOURCES ###
 '''
-https://pygis.io/docs/d_access_osm.html
+OSMNX: 
 https://github.com/gboeing/osmnx
-https://osmnx.readthedocs.io/en/stable/getting-started.html
+https://osmnx.readthedocs.io/en/stable/user-reference.html
+https://pygis.io/docs/d_access_osm.html
+
+OSM:
 https://wiki.openstreetmap.org/wiki/OSMPythonTools
-https://racum.blog/articles/osm-python/
-https://peps.python.org/pep-0008/
+https://racum.blog/articles/osm-python/0
+https://wiki.openstreetmap.org/wiki/Map_features
+
+TKinter:
 https://github.com/HezekiahMD/TkinterMapView2
+https://stackoverflow.com/questions/17466561/what-is-the-best-way-to-structure-a-tkinter-application
+https://pythonguides.com/python-tkinter-search-box/
+https://tkdocs.com/tutorial/index.html
+
+Other APIs:
+https://github.com/public-api-lists/public-api-lists
+https://publicapis.io/district-of-columbia-open-data-api
+https://www.stlouis-mo.gov/government/departments/information-technology/web-development/city-api/index.cfm
+https://data-msdis.opendata.arcgis.com/ : uses ESRI ArcGIS API https://developers.arcgis.com/rest/
+https://api.nasa.gov/
+https://docs.sentinel-hub.com/api/latest/data/sentinel-2-l2a/
 '''
 
 ### DEPENDENCIES ###
@@ -17,19 +33,31 @@ https://github.com/HezekiahMD/TkinterMapView2
 
 import tkinter as tk
 import osmnx as ox
+import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def query_osm(place_name, plot_frame):
+# need to optimize so that it doesn't need to make recurrent calls if place_name is the same
+def query_osm(place_name, plot_frame, option):
     for widget in plot_frame.winfo_children():
         widget.destroy()
     try:
+        result = gpd.GeoDataFrame()
         # CALL API
-        area = ox.geocode_to_gdf(place_name)   
+        result = ox.geocode_to_gdf(place_name)   
+        
+        if option == 'Streets':
+            tags = {'highway': True}   
+            geom = ox.features_from_polygon(result.iloc[0]['geometry'], tags)  
+        elif option == 'Buildings':
+            tags = {'building': True}   
+            geom = ox.features_from_polygon(result.iloc[0]['geometry'], tags)
+        else:
+            geom = result
 
         # CREATE PLOT
         fig, ax = plt.subplots(figsize=(6, 6))
-        area.plot(ax=ax)
+        geom.plot(ax=ax)
         ax.set_xticks([]) # remove ticks & labels
         ax.set_yticks([]) 
         ax.set_xlabel('') 
@@ -50,20 +78,34 @@ class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-         
+        
+        input_frame = tk.Frame(self)
+        input_frame.pack(fill=tk.X, pady=(0,10))
+
+        # OPTIONS
+        options = {
+            "Boundary" : "Boundary",
+            "Streets" : "Streets",
+            "Buildings" : "Buildings",
+        }
+        option_frame = tk.Frame(input_frame)
+        option_frame.pack(fill=tk.X, pady=(0,10))
+        option_label = tk.Label(option_frame, text="Options")
+        option_label.pack(side=tk.LEFT, padx=(0, 5))
+        option_var = tk.StringVar(option_frame, "Boundary")
+        for (text, value) in options.items(): 
+            tk.Radiobutton(option_frame, text = text, variable = option_var, value = value).pack(side="top", ipady = 5) 
+
         # SEARCH BOX
-        search_frame = tk.Frame(self)
+        search_frame = tk.Frame(input_frame)
         search_frame.pack(fill=tk.X, pady=(0, 10))
         search_label = tk.Label(search_frame, text="Enter place name:")
         search_label.pack(side=tk.LEFT, padx=(0, 5))
         search_var = tk.StringVar()
         search_entry = tk.Entry(search_frame, textvariable=search_var, width=50)
         search_entry.pack(side=tk.LEFT, padx=(0, 10), fill=tk.X, expand=True)
-        search_button = tk.Button(search_frame, text="Search", command=lambda: query_osm(search_var.get(), plot_area))
+        search_button = tk.Button(search_frame, text="Search", command=lambda: query_osm(search_var.get(), plot_area, option_var.get()))
         search_button.pack(side=tk.LEFT)
-
-        # FILTERS & OPTIONS
-        
 
         # OUTPUT
         plot_area = tk.Frame(self, relief=tk.SUNKEN, borderwidth=2)
